08d9b5885b31e83ee4e129cd373794640fc33f02
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2020-08-28 16:38:58 -0500

    read tweets visible to @drewwhis

---
+++ src/bot.py
@@ -0,0 +1,20 @@
+import tweepy
+import json
+
+with open('../auth/credentials.json') as f:
+  data = json.load(f)
+
+consumer_key = data['consumer_key']
+consumer_secret = data['consumer_secret']
+access_token = data['access_token']
+access_token_secret = data['access_token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
